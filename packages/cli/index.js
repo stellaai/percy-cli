a5ef31b2f7111f1ef47eb8458d6b1ae933d89dd0
Ben <bennrhodes@gmail.com>
2021-11-01 13:55:25 +1100

    tweak

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,10 +1,12 @@
 /* istanbul ignore if: hard to test without actually using an unsupported version */
 if (parseInt(process.version.split('.')[0].substring(1), 10) < 12) {
-  console.error(`Node ${process.version} is not supported. Percy only supports the current LTS of Node. Please upgrade to Node v12+`);
+  console.error(
+    `Node ${process.version} is not supported. Percy only supports the current LTS of Node. Please upgrade to Node v12+`
+  );
   process.exit(1);
 }
 
-const { promises: fs } = require('fs');
+const { promises: fs, readdirSync, statSync } = require('fs');
 const path = require('path');
 
 // find plugins in a directory, matching a pattern, ignoring registered plugins
@@ -13,23 +15,56 @@ function findPlugins(dir, pattern, registered) {
   let regexp = new RegExp(`^${segments.pop().replace('*', '.*')}`);
   dir = path.join(dir, ...segments);
 
-  return fs.readdir(dir).then(f => f.reduce((plugins, dirname) => {
-    // exclude CLI's own directory and any directory not matching the pattern
-    if (dirname === 'cli' || !regexp.test(dirname)) return plugins;
+  return fs.readdir(dir).then(
+    (f) =>
+      f.reduce((plugins, dirname) => {
+        // exclude CLI's own directory and any directory not matching the pattern
+        if (dirname === 'cli' || !regexp.test(dirname)) return plugins;
 
-    try {
-      let { name, oclif } = require(`${dir}/${dirname}/package.json`);
+        // If this is has a node_modules foldern with a @percy folder (ie. in pnpm), try
+        // reading package.json files of children
+        let directoriesToTry = [`${dir}/${dirname}`];
+        const percyModulesDir = path.resolve(dir, `${dirname}/node_modules/@percy`);
+        try {
+          const stat = statSync(percyModulesDir);
+          if (stat && stat.isDirectory()) {
+            // Get top level folders
+            const newDirs = readdirSync(percyModulesDir);
+            directoriesToTry = newDirs
+              .filter((p) => p !== 'cli')
+              .map((p) => `${dir}/${dirname}/node_modules/@percy/${p}`);
+            directoriesToTry.forEach(function (dx) {
+              try {
+                // Try to read from package.json
+                let { name, oclif } = require(`${dx}/package.json`);
 
-      // plugin's package.json have a percy oclif binary defined
-      if (!registered.includes(name) && oclif && oclif.bin === 'percy') {
-        return plugins.concat(name);
-      }
-    } catch {
-      // ignore directories without a package.json
-    }
+                // plugin's package.json have a percy oclif binary defined
+                if (!registered.includes(name) && oclif && oclif.bin === 'percy') {
+                  console.log(`Adding command ${name}`);
+                  plugins = plugins.concat(name);
+                }
+              } catch {
+                // ignore directories without a package.json
+              }
+            });
+          }
+        } catch {
+          try {
+            let { name, oclif } = require(`${dir}/${dirname}/package.json`);
 
-    return plugins;
-  }, []), () => []);
+            // plugin's package.json have a percy oclif binary defined
+            if (!registered.includes(name) && oclif && oclif.bin === 'percy') {
+              return plugins.concat(name);
+            }
+          } catch {
+            // ignore directories without a package.json
+          }
+        }
+
+        return plugins;
+      }, []),
+    () => []
+  );
 }
 
 // automatically register/unregister plugins by altering the CLI's package.json within node_modules
@@ -37,6 +72,7 @@ async function autoRegisterPlugins() {
   let nodeModules = path.resolve(__dirname, '../..');
   let pkgPath = path.resolve(__dirname, 'package.json');
   let pkg = require(pkgPath);
+  let pnpmNodeModules = path.resolve(nodeModules, '../..');
 
   // if not installed within node_modules, look within own node_modules
   /* istanbul ignore else: always true during tests */
@@ -45,9 +81,11 @@ async function autoRegisterPlugins() {
   }
 
   // ensure registered plugins can be resolved
-  let registered = pkg.oclif.plugins.filter(plugin => {
+  let registered = pkg.oclif.plugins.filter((plugin) => {
     if (pkg.dependencies[plugin]) return true;
-    try { return !!require.resolve(plugin); } catch {}
+    try {
+      return !!require.resolve(plugin);
+    } catch {}
     return false;
   });
 
@@ -55,7 +93,7 @@ async function autoRegisterPlugins() {
   let unregistered = await Promise.all([
     findPlugins(nodeModules, '@percy/*', registered),
     findPlugins(nodeModules, 'percy-cli-*', registered)
-  ]).then(p => Array.from(new Set(p.flat())));
+  ]).then((p) => Array.from(new Set(p.flat())));
 
   // if any unregistered or unresolved registered, modify plugin registry
   if (unregistered.length || registered.length !== pkg.oclif.plugins.length) {
@@ -65,5 +103,4 @@ async function autoRegisterPlugins() {
 }
 
 // auto register plugins before running oclif
-module.exports.run = () => autoRegisterPlugins()
-  .then(() => require('@oclif/command').run());
+module.exports.run = () => autoRegisterPlugins().then(() => require('@oclif/command').run());
